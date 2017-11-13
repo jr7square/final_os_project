26692279023a3ea8b568c47f97776721d95f3416
Carrillo <jcarrillo1@osnode01.csee.usf.edu>
2017-11-13 14:37:56 -0500

    woot

--- group_project.c
+++ group_project.c
@@ -165,12 +165,13 @@ void *leftQueueFunction() {
             c = deQueue(left_queue)->key;
             sem_post(&left_mutex);
             sem_wait(&rope_available);
-            pthread_create(&baboonThreads[count], &attr, baboonCrossing, (void *)&dir);
+            pthread_create(&baboonThreads[count], &attr, baboonCrossing, (void *)&c);
         }
         for (total_threads = 0; total_threads < count; total_threads++) {
             pthread_join(baboonThreads[total_threads], NULL);
         }
         sem_post(&direction_mutex);
+        sleep(1);
     }
     pthread_exit(NULL);
 }
@@ -178,6 +179,7 @@ void *leftQueueFunction() {
 void *rightQueueFunction() {
     char c;
     int count = 0;
+    int total_threads = 0;
     pthread_attr_t attr;
 
     pthread_attr_init(&attr);
@@ -191,12 +193,13 @@ void *rightQueueFunction() {
             c = deQueue(right_queue)->key;
             sem_post(&right_mutex);
             sem_wait(&rope_available);
-            pthread_create(&baboonThreads[count], &attr, baboonCrossing, (void *)&dir);
+            pthread_create(&baboonThreads[count], &attr, baboonCrossing, (void *)&c);
         }
         for (total_threads = 0; total_threads < count; total_threads++) {
             pthread_join(baboonThreads[total_threads], NULL);
         }
         sem_post(&direction_mutex);
+        sleep(1);
     }
     pthread_exit(NULL);
 }
